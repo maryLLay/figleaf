8973bdaa75adc0d5b6ef2786f6cb0e753e77496d
maryLLay <mrs.mary.lay@gmail.com>
2024-04-11 09:52:08 -0400

    Added MouseLight-specific functionality: neuron list and ML database queries

--- figleaf/create_and_publish.py
+++ figleaf/create_and_publish.py
@@ -1,28 +1,34 @@
 """
 A script to create a private article from a file containing JSON-formatted metadata for that article.
+Specific to the Mouselight Workflow.
 Offers the user the option to upload one data file to that article. 
-Articles are private before being made public. Run this script with -t and your stage token.
-Currently the stage URL is hard-coded into the script, so doesn't work for production.
+Articles are private before being made public. Run this script with -t and your production token.
+Currently the stage URL is hard-coded into the script, so doesn't work for stage.
 Write down the article ID that is printed to the terminal!
 """
 
-import requests
+import pip._vendor.requests as requests
 import argparse
 import json
 import sys
 import hashlib
 import os
+import mnb_graphQL_queries #indicates to import from current directory, as opposed to out on the web?  Probably fails if you change current working directory
 
 CHUNK_SIZE = 10 * (1024 ** 2) # e.g. 10 * (1024 ** 2) = 10 Mb.
-base_url = "https://api.figsh.com/v2/account/articles" #TODO: use --stage flag instead of hard-coding
+base_url = "https://api.figshare.com/v2/account/articles" #TODO: use --stage flag instead of hard-coding; Also, "https://api.figsh.com/v2/account/articles" = stage
 
-neurons_list = ['AA1583']
-root = ''
+brain_id = "" # eg, "f0702a2d-d242-4a93-a3a1-5c6752a4888a" from the MNBD
+neurons_list = []
+root = r'Z:\neuron-database\export\CCFv3' #where the swc and json files are located (Allen CCFv3 db folder)
 swcs = "swc30"
 jsons = "json30"
-metadata_path = r''
+metadata_path = r'C:\Users\laym\Documents\figleaf\metadata\mouselight_metadata.json' # Path to json metadata file
+data_dict = {} #data_dict[neuron] = [new_article_id, doi_res, neuron_id]   A dict of lists
+#missing_dois_dict = {} #{'AA1625': 'dc662ed0-067f-4768-847d-ac54befb22e8', 'AA1626': '6b0c60b1-91ee-40d7-b9af-7cf2f3816c4e', 'AA1627': '09a5fbeb-33b5-42e8-b623-53c574515874', 'AA1628': '79db13e6-16ca-4309-84a5-9c2d253eefdf'}
 
-print("here")
+
+
 
 def checkOK(response_to_check):
     if not response_to_check.ok:
@@ -73,7 +79,19 @@ def returnTitle(n):
     #n = string or neuron_list[index number]
     return f'MouseLight Neuron {n}'
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
+    #if neuron list is blank 
+    if neurons_list == []:
+        try:
+            neurons_dict = mnb_graphQL_queries.missingDOIs(brain_id)
+            print(neurons_dict)
+            neurons_list = mnb_graphQL_queries.process_neurons_dict(neurons_dict)
+            print(neurons_list)
+            
+        except Exception as e:
+            brain_id = input("Please enter a valid brain ID (long, alphanumeric string): ")
+
+        
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-t', 
                         '--token', 
@@ -85,50 +103,67 @@ if __name__ == "__main__":
                     help = 'If -s is included, script will grab info from the stage environment, not the production environment.',
                     action='store_true' # The store_true option automatically creates a default value of False.
                     )
+    parser.add_argument('-id',
+                    '--article_id',
+                    required = False,
+                    help = 'Article ID, which was printed to the terminal at the time of creation.'
+                    )
     args = parser.parse_args()
 
-#opens metadata file to use as template
-with open (metadata_path, "r+") as metadata_file: #new
-    data = json.load(metadata_file) #new
+    #opens metadata file to use as template
+    with open (metadata_path, "r+") as metadata_file: #new
+        data = json.load(metadata_file) #new
 
-for neuron in neurons_list:
-    pathy = os.path.join(root, swcs, neuron+".swc")
-    print(pathy)
-    if os.path.isfile(pathy):
-        swc_path = pathy
-        json_path = os.path.join(root, jsons, neuron+".json")
-        print("files exist!")
-    #TODO: what if file does not exist?
+    #for neuron in neurons_list:
+    for neuron, neuron_id in neurons_dict.items():
 
-
-    #Modify the metadata field 'title' to reflect the appropriate neuron name
-    data['title'] = returnTitle("MouseLight Neuron "+ neuron) #new
-    s = json.dumps(data)
-    with open ("tempfile.json", "w") as outfile:
-        outfile.write(s)
-    #Data must be in binary format in order for code below to work
-    with open ("tempfile.json", "rb") as infile:
-        data_bin = infile.read() #new
-    
-    #Submit post request
-    headers = {'Authorization': 'token {}'.format(args.token)}
-    response = requests.post(base_url, headers=headers, data=data_bin)  #data=data was the old method
-    checkOK(response)
-    new_article_id = json.loads(response.content)['entity_id']
-    print(f"New private article with ID {new_article_id} successfully created from {metadata_file}.")  #stop here and check figshare
-
-    #doi = input('Would you like to reserve a DOI for this article? (y/n): ')
-    doi = "n" #new
-    if doi.lower() == 'y':
-        doi_res = requests.post(f"{base_url}/{args.article_id}/reserve_doi", headers=headers)
-        checkOK(doi_res)
-        print("DOI reserved successfully.")
-
-    proceed = "y"
-    while True:
-        #proceed = input(f"Would you like to upload a file to the article you just created? (y/n): ")
+        #Modify the metadata field 'title' to reflect the appropriate neuron name
+        data['title'] = returnTitle("MouseLight Neuron "+ neuron) #new
+        s = json.dumps(data)
+        with open ("tempfile.json", "w") as outfile:
+            outfile.write(s)
+        #Data must be in binary format in order for code below to work
+        with open ("tempfile.json", "rb") as infile:
+            data_bin = infile.read() #new
         
+        #Submit post request to make article
+        headers = {'Authorization': 'token {}'.format(args.token)}
+        response = requests.post(base_url, headers=headers, data=data_bin)  #data=data was the old method
+        checkOK(response)
+        new_article_id = json.loads(response.content)['entity_id']
+        print(f"New private article with ID {new_article_id} successfully created from {metadata_file}.")  #stop here and check figshare
+
+        #doi = input('Would you like to reserve a DOI for this article? (y/n): ')
+        doi = "y" #new
+        if doi.lower() == 'y':
+            #doi_res = requests.post(f"{base_url}/{args.article_id}/reserve_doi", headers=headers)
+            doi_res = requests.post(f"{base_url}/{new_article_id}/reserve_doi", headers=headers)
+            checkOK(doi_res)
+            print("DOI reserved successfully.")
+            doi_num = (json.loads(doi_res.content))["doi"]
+            print("neuron: " + neuron +"doi: "+ doi_num + "article id: " + str(new_article_id))  #This needs to be saved as a dict
+            data_dict[neuron] = [str(new_article_id), doi_num, neuron_id]
+
+        print(data_dict)
+
+
+        proceed = "n"
+
+        #proceed = input(f"Would you like to upload a file to the article you just created? (y/n): ")
+
         if proceed.lower() == "y":
+            # Check that upload files exist before proceeding to upload step
+            pathy = os.path.join(root, swcs, neuron+".swc")
+            print(pathy)
+            if os.path.isfile(pathy):
+                swc_path = pathy
+                json_path = os.path.join(root, jsons, neuron+".json")
+                print("files exist!")
+            else:
+                print ("files do not exist!")
+            #TODO: what if file does not exist?                
+            #TODO: this part should be moved to later, esp since we don't upload files til later
+
             file_info = initiate_new_upload(base_url, headers, json.loads(response.content)['entity_id'], swc_path, neuron+'.swc')
             print("Uploading file ", swc_path, f"in {CHUNK_SIZE/(1024 ** 2)}Mb chunks")
             # Until here we used the figshare API; the following lines use the figshare upload service API.
@@ -137,7 +172,7 @@ for neuron in neurons_list:
             up_res = requests.post(file_info['location'], headers=headers)
             checkOK(up_res)
             print("Upload successful.")
-            
+                
             #upload json file
             file_info = initiate_new_upload(base_url, headers, json.loads(response.content)['entity_id'], json_path, neuron+'.json')
             print("Uploading file ", json_path, f"in {CHUNK_SIZE/(1024 ** 2)}Mb chunks")
@@ -148,14 +183,20 @@ for neuron in neurons_list:
             checkOK(up_res)
             print("Upload successful.")
             proceed = "n"
+
         elif proceed.lower() == "n":
             break  # break out of the while loop
         else:
             print("Invalid input. Please enter 'y' to upload a file, or 'n' to cancel.")
 
-    #publish = input(f"Would you like to publish this article now? (y/n): ")
-    publish = "n" #new
-    if publish.lower() == 'y':
-        pub_res = requests.post(f"{base_url}/{args.article_id}/publish", headers=headers)
-        checkOK(pub_res)
-        print("Article published successfully.")
+        #publish = input(f"Would you like to publish this article now? (y/n): ")
+        publish = "n" #new
+        if publish.lower() == 'y':
+            pub_res = requests.post(f"{base_url}/{args.article_id}/publish", headers=headers)
+            checkOK(pub_res)
+            print("Article published successfully.")
+
+    # Upload dois to Mouselight Database
+    mnb_graphQL_queries.upload_dois(data_dict)
+
+print(data_dict)
